5be604635113ba840ceae198d118adb201860a47
Mahmudul Alam <mahmudula2000@gmail.com>
2020-12-17 15:26:40 +0600

    Solved Bug

--- attendance.js
+++ attendance.js
@@ -1,28 +1,29 @@
 console.log("On Action")
 chrome.runtime.onMessage.addListener(startIt);
-const totalAttendance = [];
+let totalAttendance = [];
+let interval = [];
+let intervalId = 0;
 let today = new Date();
 let month = today.getMonth();
-let day = today.getDate();
+let year = today.getFullYear();
+let day = today.getDay();
 let hours = today.getHours();
 let file_name = "Attendance:" + hours + "hrs-" + day + "-" + month + ".txt";
 
 function startIt(message, sender, sendResponse) {
     console.log(message);
-    let startRec;
     if (message[0] === 1) {
-        startRec = setInterval(takeAttendance, 60000);
+        interval[intervalId++] = setInterval(takeAttendance, 10000);
     } else {
-        clearInterval(startRec);
-        console.log("Saving")
+        for (let i = 0; i < interval.length; i++) {
+            clearInterval(interval[i]);
+        }
         saveToFile();
     }
 }
 
 function takeAttendance() {
-    console.log("Taking Attendance");
     if (document.domain === "meet.google.com") {
-        console.log("Using Google meet");
         meetAttendance();
     }
     if (document.domain === "zoom.us" || document.domain === "us04web.zoom.us") {
@@ -36,7 +37,6 @@ const meetAttendance = () => {
     for (let i = 0; i < presentStudents.length; i++) {
         currentAttendance.push(presentStudents[i].innerHTML)
     }
-    console.log("currentAttendance", currentAttendance)
     totalAttendance.push(currentAttendance)
 }
 
@@ -44,8 +44,39 @@ const zoomAttendance = () => {
 
 }
 
+const organizeData = () => {
+    console.table(totalAttendance);
+    const allStudents = [];
+    for (let i = 0; i < totalAttendance.length; i++) {
+        if (totalAttendance[i] !== undefined) {
+            for (let j = 0; j < totalAttendance[i].length; j++) {
+                console.log("Running0")
+                if (allStudents.indexOf(totalAttendance[i][j]) === -1) allStudents.push(totalAttendance[i][j]);
+            }
+        }
+    }
+    console.log("Running1")
+    let text = "Attendance of " + year + "-" + day + "-" + month + "hrs-" + hours + "\n";
+    let arr = [text];
+    for (let i = 0; i < allStudents.length; i++) {
+        console.log("Running2");
+        text = allStudents[i];
+        for (let j = 0; j < totalAttendance.length; j++) {
+            let found = 0;
+            console.log("Array part", totalAttendance[i]);
+            if (totalAttendance[i].indexOf(allStudents[i]) !== -1) found = 1;
+            text += "," + found;
+        }
+        arr.push(text);
+    }
+    console.log(arr);
+    return arr;
+}
+
 function saveToFile() {
-    const pData = new Blob(totalAttendance, {type: 'text/plain'});
+    const text = organizeData();
+    console.log("Running3")
+    const pData = new Blob(text, {type: 'text/plain'});
     const a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
