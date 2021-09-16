4fe3f8cd19bd6ad5809b23091e08bfe6246df9b3
Mahmudul Alam <mahmudula2000@gmail.com>
2021-09-16 08:37:31 +0600

    added muted, video off list for zoom

--- attendance.js
+++ attendance.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(startIt);
 let totalAttendance = [];
+let rangeMembers = [];
 let interval = [];
 let intervalId = 0;
 let today = new Date();
@@ -9,91 +10,174 @@ let day = today.getDay();
 let hours = today.getHours();
 let file_name = "Attendance:" + hours + "hrs-" + day + "-" + month + ".txt";
 
+window.onload = function () {
+  window.sessionStorage.clear();
+  chrome.storage.local.clear();
+}
+
 function startIt(message, sender, sendResponse) {
-    if (message[0] === 1) {
-        console.log("Taking attendance");
-        interval[intervalId++] = setInterval(takeAttendance, 10000);
-    } else {
-        for (let i = 0; i < interval.length; i++) {
-            clearInterval(interval[i]);
-        }
-        saveToFile();
+  if (message[0] === 1) {
+    console.log("Taking attendance");
+    interval[intervalId++] = setInterval(takeAttendance, 1000);
+  } else if (message[0] === 2) {
+    for (let i = 0; i < interval.length; i++) {
+      clearInterval(interval[i]);
     }
+    saveToFile();
+  } else {
+    console.log(message);
+  }
 }
 
 const takeAttendance = () => {
-    if (document.domain === "meet.google.com") {
-        console.log("Taking attendance from meet")
-        meetAttendance();
-    }
-    if (document.domain === "us04web.zoom.us" || document.domain === "bdren.zoom.us") {
-        console.log("Taking attendance from zoom")
-        zoomAttendance();
-    }
+  if (document.domain === "meet.google.com") {
+    console.log("Taking attendance from meet")
+    meetAttendance();
+  }
+  if (document.domain === "us04web.zoom.us" || document.domain === "bdren.zoom.us") {
+    console.log("Taking attendance from zoom")
+    zoomAttendance();
+  }
 }
 
 const meetAttendance = () => {
-    const currentAttendance = [];
-    const presentStudents = document.getElementsByClassName("ZjFb7c");
-    if (presentStudents.length === 0) {
-        alert("Please show participants list")
-    } else {
-        for (let i = 0; i < presentStudents.length; i++) {
-            currentAttendance.push(presentStudents[i].innerHTML)
-        }
-        totalAttendance.push(currentAttendance);
-    }
+  const currentAttendance = [];
+  let memberList = document.getElementsByClassName("KV1GEc");
+  if (memberList.length === 0) {
+    document.getElementsByClassName("VfPpkd-Bz112c-Jh9lGc")[8].click();
+    memberList = document.getElementsByClassName("KV1GEc");
+  }
+  for (let member of memberList) {
+    let memberName = member.getElementsByClassName("ZjFb7c")[0].innerText;
+    currentAttendance.push(memberName)
+  }
+  totalAttendance.push(currentAttendance);
+  genAbsentMembers(currentAttendance);
+  genExtraMembers(currentAttendance);
 }
 
 const zoomAttendance = () => {
-    const currentAttendance = [];
-    const presentStudents = document.getElementsByClassName("participants-item__display-name");
-    if (presentStudents.length === 0) {
-        alert("Please show participants list")
-    } else {
-        for (let i = 0; i < presentStudents.length; i++) {
-            currentAttendance.push(presentStudents[i].innerHTML)
-        }
-        totalAttendance.push(currentAttendance);
+  const currentAttendance = [];
+  let muted = [];
+  let noVideo = [];
+  let memberList = document.getElementsByClassName("participants-item__item-layout");
+  if (memberList.length === 0) {
+    document.getElementsByClassName("footer-button__participants-icon")[0].click();
+    memberList = document.getElementsByClassName("participants-item__item-layout");
+  }
+  for (let student of memberList) {
+    let studentName = student.getElementsByClassName("participants-item__display-name")[0].innerText;
+    currentAttendance.push(studentName);
+    if (student.getElementsByClassName("participants-icon__participants-unmute").length !== 0) {
+      muted.push(studentName);
     }
+    if (student.getElementsByClassName("participants-icon__participant-video--started participants-icon__participant-video").length === 0) {
+      noVideo.push(studentName);
+    }
+  }
+  totalAttendance.push(currentAttendance);
+  saveMuted(muted);
+  saveNoVideo(noVideo);
+  genAbsentMembers(currentAttendance);
+  genExtraMembers(currentAttendance);
+}
+
+const getRangeMembers = (start, end, included, excluded) => {
+  for (let i = start; i <= end; i++) {
+    rangeMembers.push(i);
+  }
+  for (let i of included.split(",")) {
+    rangeMembers.push(i);
+  }
+  for (let i of excluded.split(",")) {
+
+  }
+}
+
+const genAbsentMembers = (currentMembers) => {
+  const absentMembers = [];
+  for (let member of rangeMembers) {
+    if (currentMembers.indexOf(member) === -1) {
+      absentMembers.push(member);
+    }
+  }
+  saveAbsent(absentMembers);
+}
+
+const genExtraMembers = (currentMembers) => {
+  const extraMembers = [];
+  for (let member of currentMembers) {
+    if (rangeMembers.indexOf(member) === -1) {
+      extraMembers.push(member);
+    }
+  }
+  saveExtra(extraMembers);
+}
+
+function saveMuted(mutedList = []) {
+  let data = {arr: mutedList};
+  console.log(mutedList);
+  chrome.runtime.sendMessage({mutedList: JSON.stringify(data)}, function (response) {
+  });
+}
+
+function saveNoVideo(mutedList = []) {
+  let data = {arr: mutedList};
+  console.log(mutedList);
+  chrome.runtime.sendMessage({noVideo: JSON.stringify(data)}, function (response) {
+  });
+}
+
+function saveAbsent(mutedList = []) {
+  let data = {arr: mutedList};
+  console.log(mutedList);
+  chrome.runtime.sendMessage({absentMembers: JSON.stringify(data)}, function (response) {
+  });
+}
+
+function saveExtra(mutedList = []) {
+  let data = {arr: mutedList};
+  console.log(mutedList);
+  chrome.runtime.sendMessage({extraMembers: JSON.stringify(data)}, function (response) {
+  });
 }
 
 const organizeData = () => {
-    console.table(totalAttendance);
-    const allStudents = [];
-    for (let i = 0; i < totalAttendance.length; i++) {
-        if (totalAttendance[i] !== undefined) {
-            for (let j = 0; j < totalAttendance[i].length; j++) {
-                if (allStudents.indexOf(totalAttendance[i][j]) === -1) allStudents.push(totalAttendance[i][j]);
-            }
-        }
+  console.table(totalAttendance);
+  const allStudents = [];
+  for (let i = 0; i < totalAttendance.length; i++) {
+    if (totalAttendance[i] !== undefined) {
+      for (let j = 0; j < totalAttendance[i].length; j++) {
+        if (allStudents.indexOf(totalAttendance[i][j]) === -1) allStudents.push(totalAttendance[i][j]);
+      }
     }
-    let text = "Attendance of " + year + "-" + day + "-" + month + "hrs-" + hours + "\n";
-    let arr = [text];
-    for (let i = 0; i < allStudents.length; i++) {
-        text = allStudents[i];
-        for (let j = 0; j < totalAttendance.length; j++) {
-            let found = 0;
-            if (totalAttendance[j].indexOf(allStudents[i]) !== -1) found = 1;
-            text += "," + found;
-        }
-        text += '\n';
-        arr.push(text);
+  }
+  let text = "Attendance of " + year + "-" + day + "-" + month + "hrs-" + hours + "\n";
+  let arr = [text];
+  for (let i = 0; i < allStudents.length; i++) {
+    text = allStudents[i];
+    for (let j = 0; j < totalAttendance.length; j++) {
+      let found = 0;
+      if (totalAttendance[j].indexOf(allStudents[i]) !== -1) found = 1;
+      text += "," + found;
     }
-    return arr;
+    text += '\n';
+    arr.push(text);
+  }
+  return arr;
 }
 
 const saveToFile = () => {
-    const text = organizeData();
-    console.log("Running3")
-    const pData = new Blob(text, {type: 'text/plain'});
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    a.href = URL.createObjectURL(pData);
-    a.download = file_name;
-    a.click();
+  const text = organizeData();
+  console.log("Running3")
+  const pData = new Blob(text, {type: 'text/plain'});
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = URL.createObjectURL(pData);
+  a.download = file_name;
+  a.click();
 
-    URL.revokeObjectURL(a.href)
-    a.remove();
+  URL.revokeObjectURL(a.href)
+  a.remove();
 }
--- popup.js
+++ popup.js
@@ -1,48 +1,109 @@
-load();
+chrome.runtime.onMessage.addListener(handleMessage);
 let bgpage = chrome.extension.getBackgroundPage();
 if (bgpage.word === "Page being used is ZOOM") {
-    document.getElementById("time_div").style.display = "none";
+  document.getElementById("time_div").style.display = "none";
 }
 let params = {
-    active: true,
-    currentWindow: true
+  active: true,
+  currentWindow: true
 }
-const config = 0;
 
-function load() {
-    chrome.storage.local.get('given_len', function (result) {
-        let roll_len1 = result.given_len;
-        //alert(result.given_len);
-        const elem = document.querySelector('#rolllen');
-        if (roll_len1 !== undefined) {
-            elem.setAttribute("placeholder", roll_len1);
-            elem.setAttribute("value", roll_len1);
-        }
-    });
+const buttonList = ["mic", "video", "people-cross", "people-plus", "settings"];
+
+function handleMessage(message, sender, sendResponse) {
+  const showing = window.sessionStorage.getItem("showing");
+  if (showing === "0" && message.mutedList) {
+    renderMuted(JSON.parse(message.mutedList).arr);
+  } else if (showing === "1" && message.noVideo) {
+    renderMuted(JSON.parse(message.noVideo).arr);
+  } else if (showing === "2" && message.absentMembers) {
+    renderMuted(JSON.parse(message.absentMembers).arr);
+  } else if (showing === "3" && message.extraMembers) {
+    renderMuted(JSON.parse(message.extraMembers).arr);
+  }
+  sendResponse({status: true});
 }
 
 window.onload = function () {
+  document.getElementById("startButton").addEventListener("click", function () {
+    const data = [];
+    chrome.tabs.query(params, doIt);
+    data.push(1);
 
-    document.getElementById("startButton").addEventListener("click", function () {
-        const data = [];
-        chrome.tabs.query(params, doIt);
-        data.push(1);
+    function doIt(tabs) {
+      chrome.storage.local.set({running: "1"}, function () {
+      });
+      document.getElementById('startButton').style.display = "none";
+      document.getElementById('stopButton').style.display = "block";
+      document.getElementById('nav-btn').style.display = "block";
+      chrome.tabs.sendMessage(tabs[0].id, data);
+    }
+  });
 
-        function doIt(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, data);
-            window.close();
-        }
+  for (let button of buttonList) {
+    document.getElementById(button).addEventListener("click", function () {
+      inactiveAll();
+      window.sessionStorage.setItem("showing", buttonList.indexOf(button).toString())
+      document.getElementById(button).classList.add("active");
+      if (button === "settings") {
+        document.getElementById("setting-form").style.display = "block";
+      }
     });
+  }
 
-    document.getElementById("stopButton").addEventListener("click", function () {
-        const data = [];
-        chrome.tabs.query(params, doIt);
-        data.push(2);
+  document.getElementById("stopButton").addEventListener("click", function () {
+    const data = [];
+    chrome.tabs.query(params, doIt);
+    data.push(2);
 
-        function doIt(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, data);
-            window.close();
-        }
-    });
+    function doIt(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, data);
+      chrome.storage.local.set({running: "0"}, function () {
+      });
+      document.getElementById('startButton').style.display = "block";
+      document.getElementById('stopButton').style.display = "none";
+      document.getElementById('nav-btn').style.display = "none";
+    }
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // onClick's logic below:
+  chrome.storage.local.get(function (result) {
+    const running = result.running;
+    if (running === "1") {
+      document.getElementById('startButton').style.display = "none";
+      document.getElementById('stopButton').style.display = "block";
+      document.getElementById('nav-btn').style.display = "block";
+    } else {
+      document.getElementById('startButton').style.display = "block";
+      document.getElementById('stopButton').style.display = "none";
+      document.getElementById('nav-btn').style.display = "none";
+    }
+  });
+});
+
+function inactiveAll() {
+  for (let button of buttonList) {
+    document.getElementById(button).classList.remove("active");
+  }
+}
+
+function renderMuted(arr) {
+  document.getElementById("setting-form").style.display = "none";
+  let parent = document.getElementById("live-result");
+  parent.innerHTML = "";
+  if (arr.length !== 0) {
+    for (let ele of arr) {
+      let child = document.createElement('p');
+      child.innerText = ele;
+      parent.appendChild(child);
+    }
+  } else {
+    let child = document.createElement('p');
+    child.innerText = "No member(s)"
+    parent.appendChild(child);
+  }
 }
 
